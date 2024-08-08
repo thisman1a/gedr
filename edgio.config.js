f3e7fd7c149aee22c9f8b02774c44953a3396c69
thisman1a <138527769+thisman1a@users.noreply.github.com>
2024-08-08 18:16:52 +0330

    Update edgio.config.js

---
+++ edgio.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  backends: {
+    xrayServer: {
+      domainOrIp: '213.238.182.113',
+      port: 9001,
+      protocol: 'http',
+      hostHeader: '213.238.182.113',
+    },
+  },
+}
